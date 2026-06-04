30fa78a9003f47b271dd571c6bb4e0715f01d742
MSA! <msaprogramer@gmail.com>
2026-06-04 11:10:12 +0330

    Structure

--- integrations/laravel/NotifyServiceProvider.php
+++ integrations/laravel/NotifyServiceProvider.php
@@ -8,8 +8,10 @@ class NotifyServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $root = dirname(__DIR__, 2);
+
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/notify.php',
+            $root . '/config/notify.php',
             'Notify'
         );
 
@@ -22,25 +24,27 @@ class NotifyServiceProvider extends ServiceProvider
         $this->app->alias('Notify', NotifyManager::class);
 
         // بارگذاری helper functions
-        if (file_exists($helpers = __DIR__ . '/helpers.php')) {
+        if (file_exists($helpers = $root . '/src/helpers.php')) {
             require_once $helpers;
         }
     }
 
     public function boot(): void
     {
+        $root = dirname(__DIR__, 2);
+
         // انتشار config
         $this->publishes([
-            __DIR__ . '/../config/notify.php' => config_path('Notify.php'),
+            $root . '/config/notify.php' => config_path('Notify.php'),
         ], 'Notify-config');
 
         // انتشار migrations
         $this->publishes([
-            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+            $root . '/db/migrations/' => database_path('migrations'),
         ], 'Notify-migrations');
 
         // لود migrations به صورت خودکار
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom($root . '/db/migrations');
 
         // ثبت artisan commands
         if ($this->app->runningInConsole()) {
